fd4ea4a4900fb36634a54cb4a3a96093220dda72
Ky Phan <ky.phan@eastagile.com>
2024-03-28 15:19:40 +0700

    WIP - Add team and project

--- src/linear.js
+++ src/linear.js
@@ -1,26 +1,46 @@
-const { supabaseTachio } = require("./supabaseclient");
+const {supabaseTachio} = require("./supabaseclient");
 
-const LINEAR_TABLE_NAME = "linear_webhooks";
+const WEBHOOK_TABLE_NAME = "linear_webhooks";
+const PROJECT_TABLE_NAME = "linear_projects";
+const TEAM_TABLE_NAME = "linear_teams";
 
 async function processLinearRequest(body) {
-  const { error } = await supabaseTachio.from(LINEAR_TABLE_NAME).insert(processWebhookPayload(body));
-  if (error) throw new Error(error.message)
+    if (body.data.project) {
+        const {error} = await supabaseTachio.from(PROJECT_TABLE_NAME).upsert({
+            id: body.data.project.id,
+            name: body.data.project.name,
+        }, {onConflict: 'id', ignoreDuplicates: true});
+        if (error) throw new Error(error.message)
+    }
+
+    if (body.data.team) {
+        const {error} = await supabaseTachio.from(TEAM_TABLE_NAME).upsert({
+            id: body.data.team.id,
+            key: body.data.team.key,
+            name: body.data.team.name,
+        }, {onConflict: 'id', ignoreDuplicates: true});
+        if (error) throw new Error(error.message)
+    }
+    const {error} = await supabaseTachio.from(WEBHOOK_TABLE_NAME).insert(processWebhookPayload(body));
+    if (error) throw new Error(error.message)
 }
 
 function processWebhookPayload(payload) {
-  return {
-    updated_from: payload.updatedFrom,
-    action: payload.action,
-    actor: payload.actor,
-    created_at: payload.createdAt,
-    data: payload.data,
-    type: payload.type,
-    organization_id: payload.organizationId,
-    webhook_timestamp: new Date(payload.webhookTimestamp),
-    webhook_id: payload.webhookId,
-  }
+    return {
+        updated_from: payload.updatedFrom,
+        action: payload.action,
+        actor: payload.actor,
+        created_at: payload.createdAt,
+        data: payload.data,
+        type: payload.type,
+        organization_id: payload.organizationId,
+        webhook_timestamp: new Date(payload.webhookTimestamp),
+        webhook_id: payload.webhookId,
+        project_id: payload.data.project.id,
+        team_id: payload.data.team.id,
+    }
 }
 
 module.exports = {
-  processLinearRequest,
+    processLinearRequest,
 };
